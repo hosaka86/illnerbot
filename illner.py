44ec6169c00064d3ca79e90357cf7bfcdfab7bc5
hosaka86 <64740514+hosaka86@users.noreply.github.com>
2021-05-08 03:29:02 +0200

    Create illner.py

---
+++ illner.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import urllib.request
+from gazpacho import Soup
+import time
+
+
+landing_page = urllib.request.urlopen("https://www.zdf.de/politik/maybrit-illner")
+soup = BeautifulSoup(landing_page, "html.parser")
+
+link_list = []
+
+for link in soup.findAll('a'):
+    link_list.append(link.get('href'))
+
+followup = f"https://www.zdf.de{link_list[27]}" #get last from "Alle Videos der Sendung"
+
+get_guests = urllib.request.urlopen(followup)
+guest_soup = BeautifulSoup(get_guests, "html.parser")
+
+print(guest_soup.find("meta", {"property": "og:title"}).attrs['content'])
+print(guest_soup.find("meta", {"property": "og:description"}).attrs['content'])
+time.sleep(4)
+print(f"Mehr Infos: {followup}")
